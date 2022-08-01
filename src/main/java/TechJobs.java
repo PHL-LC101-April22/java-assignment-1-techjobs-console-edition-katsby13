3c08398b7700fe27023ee1f672a4e606e62ec2d8
Katlyn McDonald <kkmcoding@gmail.com>
2022-08-01 01:42:02 -0400

    Finished printJobs method

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,15 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+        }
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("\n*****");
+            for (Map.Entry<String, String> column : job.entrySet()) {
+                System.out.println(String.format("%s: %s,", column.getKey(), column.getValue()));
+            }
+            System.out.println("*****");
+        }
     }
 }
